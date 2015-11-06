a94d4adcdcdaa29b79d95a960383be6b2420eb92
Tho Nguyen <tho@uber.com>
2015-11-06 08:51:53 +0100

    Remove ip for listening on

--- server.js
+++ server.js
@@ -2,17 +2,15 @@
 
 // get port from environment settings for deployment on Heroku
 var EXPRESS_PORT = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 4000;
-var EXPRESS_IPADDR = process.env.OPENSHIFT_NODEJS_IP || process.env.IPADDR || '127.0.0.1';
 var EXPRESS_ROOT = __dirname;
 
-function startExpress(root, port, ipaddr) {
+function startExpress(root, port) {
   var express = require('express');
   var app = express();
   app.use(express.static(root));
-  app.listen(port, ipaddr, function() {
-    console.log('Listening on %s:%d',
-		ipaddr, port);
+  app.listen(port, function() {
+    console.log('Listening on %s:%d', port);
   });
 }
 
-startExpress(EXPRESS_ROOT, EXPRESS_PORT, EXPRESS_IPADDR);
+startExpress(EXPRESS_ROOT, EXPRESS_PORT);
